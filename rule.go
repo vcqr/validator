45776c6ce2e5dc4099c5efecab0dbbb1ed259a5e
vcqr <xingyike@126.com>
2019-01-14 23:26:50 +0800

    add rule fuc

--- rule.go
+++ rule.go
@@ -2,6 +2,7 @@ package govalidator
 
 import (
 	_ "fmt"
+	"net"
 	"reflect"
 	"strconv"
 	"strings"
@@ -15,6 +16,29 @@ func NewRule() *Rules {
 	rule := &Rules{}
 
 	return rule
+}
+
+func (this *Rules) IsNull(str string) bool {
+	return len(str) == 0
+}
+
+// Numeric check if the string contains only numbers. Empty string is valid.
+func (this *Rules) Numeric(ruleVal, fieldType string, fieldVal reflect.Value) bool {
+	if fieldType == "int" {
+		return true
+	}
+
+	if fieldType == "string" {
+		str := fieldVal.String()
+
+		if this.IsNull(str) {
+			return false
+		}
+
+		return rxNumeric.MatchString(str)
+	}
+
+	return false
 }
 
 func (this *Rules) In(ruleVal, fieldType string, fieldVal reflect.Value) bool {
@@ -70,6 +94,8 @@ func (this *Rules) Min(ruleVal, fieldType string, fieldVal reflect.Value) bool {
 		if fVal < float64(val) {
 			return false
 		}
+	} else if fieldType == "" {
+
 	} else { // 其他默认整型
 		val, err := strconv.ParseInt(ruleVal, 10, 64)
 		if err != nil {
@@ -152,4 +178,75 @@ func (this *Rules) Required(ruleVal, fieldType string, fieldVal reflect.Value) b
 
 func (this *Rules) Sometimes(ruleVal, fieldType string, fieldVal reflect.Value) bool {
 	return true
+}
+
+func (this *Rules) IsPort(ruleVal, fieldType string, fieldVal reflect.Value) bool {
+	var port int64 = 0
+	if fieldType == "string" {
+		str := fieldVal.String()
+
+		// 需要纯数字
+		if rxNumeric.MatchString(str) == false {
+			return false
+		}
+
+		val, err := strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		port = val
+	} else if fieldType == "int" {
+		port = fieldVal.Int()
+	}
+
+	if port > 0 && port < 65536 {
+		return true
+	}
+
+	return false
+}
+
+func (this *Rules) IsIPv4(ruleVal, fieldType string, fieldVal reflect.Value) bool {
+	if fieldType == "string" {
+		str := fieldVal.String()
+		ip := net.ParseIP(str)
+
+		return ip != nil && strings.Contains(str, ".")
+	}
+
+	return false
+}
+
+func (this *Rules) IsIPv6(ruleVal, fieldType string, fieldVal reflect.Value) bool {
+	if fieldType == "string" {
+		str := fieldVal.String()
+		ip := net.ParseIP(str)
+
+		return ip != nil && strings.Contains(str, ".")
+	}
+
+	return false
+}
+
+func (this *Rules) IsMAC(ruleVal, fieldType string, fieldVal reflect.Value) bool {
+	if fieldType == "string" {
+		_, err := net.ParseMAC(fieldVal.String())
+		return err == nil
+	}
+
+	return false
+}
+
+func (this *Rules) IsSSN(ruleVal, fieldType string, fieldVal reflect.Value) bool {
+	if fieldType == "string" {
+		str := fieldVal.String()
+		if str == "" || len(str) != 11 {
+			return false
+		}
+
+		return rxSSN.MatchString(str)
+	}
+
+	return false
 }
