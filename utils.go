7adb3f6576b8d5eca97daedbde7e0f385d383df2
Weichang Yang <xingyike@126.com>
2019-01-24 19:04:23 +0800

    增加类型映射方法

--- utils.go
+++ utils.go
@@ -33,15 +33,55 @@ func Ucfirst(str string) string {
 // 根据具体的字符类型，返回相关的字符串
 // int, float类型统一返回 numeric
 func getType(strType string) string {
-	var retType string
+	retType := ""
 
 	switch strType {
 	case "int", "uint", "byte", "uintptr", "rune", "uint8", "uint16", "uint32", "uint64", "int8", "int16", "int32", "int64":
 		retType = "numeric"
-	case "float32", "float64", "complex64", "complex128":
+	case "float", "float32", "float64", "complex64", "complex128":
 		retType = "numeric"
 	default:
 		retType = strType
+	}
+
+	return retType
+}
+
+// 根据类型进行映射
+// int, float类型统一返回 numeric
+func getTypeMapping(strType string) string {
+	retType := ""
+
+	// 匹配切片或者数组 格式：[\d]type
+	regSlice := "\\[\\d{0,}\\].*$"
+	rxSlice := regexp.MustCompile(regSlice)
+	if rxSlice.MatchString(strType) {
+		return "array"
+	}
+
+	// 匹配map 格式：map[type]开头的类型
+	regMap := "^map\\[[a-zA-Z]+\\].*$"
+	rxMap := regexp.MustCompile(regMap)
+	if rxMap.MatchString(strType) {
+		return "map"
+	}
+
+	// 匹配Channel 格式：chan开头的类型
+	regChan := "^chan.*$"
+	rxChan := regexp.MustCompile(regChan)
+	if rxChan.MatchString(strType) {
+		return "chan"
+	}
+
+	switch strType {
+	case "int", "uint", "byte", "uintptr", "rune", "uint8", "uint16", "uint32", "uint64", "int8", "int16", "int32", "int64":
+		retType = "int"
+	case "float", "float32", "float64", "complex64", "complex128":
+		retType = "float"
+	case "string":
+		retType = "string"
+	default:
+		retType = "unknown"
 	}
 
 	return retType
