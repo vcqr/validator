58953c5f54dc7486553a5ac2908fa2673ab5dab7
vcqr <xingyike@126.com>
2022-04-18 14:41:41 +0800

    fix: 调整映射关系

--- utils.go
+++ utils.go
@@ -40,6 +40,8 @@ func getType(strType string) string {
 		retType = "numeric"
 	case "float", "float32", "float64", "complex64", "complex128":
 		retType = "numeric"
+	case "slice", "array":
+		retType = "array"
 	default:
 		retType = strType
 	}
@@ -52,27 +54,6 @@ func getType(strType string) string {
 func getTypeMapping(strType string) string {
 	retType := ""
 
-	// 匹配切片或者数组 格式：[\d]type
-	regSlice := "\\[\\d{0,}\\].*$"
-	rxSlice := regexp.MustCompile(regSlice)
-	if rxSlice.MatchString(strType) {
-		return "array"
-	}
-
-	// 匹配map 格式：map[type]开头的类型
-	regMap := "^map\\[[a-zA-Z]+\\].*$"
-	rxMap := regexp.MustCompile(regMap)
-	if rxMap.MatchString(strType) {
-		return "map"
-	}
-
-	// 匹配Channel 格式：chan开头的类型
-	regChan := "^chan.*$"
-	rxChan := regexp.MustCompile(regChan)
-	if rxChan.MatchString(strType) {
-		return "chan"
-	}
-
 	switch strType {
 	case "int", "uint", "byte", "uintptr", "rune", "uint8", "uint16", "uint32", "uint64", "int8", "int16", "int32", "int64":
 		retType = "int"
@@ -80,6 +61,12 @@ func getTypeMapping(strType string) string {
 		retType = "float"
 	case "string":
 		retType = "string"
+	case "slice", "array":
+		retType = "array"
+	case "map":
+		retType = strType
+	case "chan":
+		retType = strType
 	default:
 		retType = "unknown"
 	}
